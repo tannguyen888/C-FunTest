f2d9d8dc95b3c8ad1e3f079fa88e8681c77ba81c
tannguyen888 <triumphdash00@gmai.com>
2026-03-18 16:54:05 -0400

    Initial commit with Bank project and tests

---
+++ tests/BankApp.Tests/AccountTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAppNS;
+using System.Transactions;
+
+namespace BankApp.Tests;
+
+[TestClass]
+public class AccountTest
+{
+    [TestMethod]
+    public void Credit_WithValidAmount_UpdatesBalance()
+    {
+        // Arrange
+        double beginBalance = 11.99;
+        double creditAmount = 5.77;
+
+        BankAccount account = new BankAccount("Tan Phat Nguyen", beginBalance);
+
+        account.Credit(creditAmount);
+        double expected = 17.76;
+        double actual = account.Balance;
+
+        Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+    }
+
+    [TestMethod]
+    public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+    {
+        double beginBalance = 15.99;
+        double creditErrorRig = -100;
+        BankAccount account = new BankAccount("Tan Phat Nguyen", 15.99);
+
+      
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            account.Credit(creditErrorRig)
+        );
+    }
+}
